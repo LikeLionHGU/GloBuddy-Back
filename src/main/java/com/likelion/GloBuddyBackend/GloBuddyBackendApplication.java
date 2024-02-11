085d6322bfd18cc99f00ddc8757c8254b6106ac5
K-Minhyeok <gurdl2384@naver.com>
2024-02-11 22:44:14 +0900

    init: 자 선수입장 ~

---
+++ src/main/java/com/likelion/GloBuddyBackend/GloBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.GloBuddyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloBuddyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/GloBuddyBackend/GloBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.GloBuddyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
