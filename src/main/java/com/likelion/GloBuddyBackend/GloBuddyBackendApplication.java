4a8e7cdf5df4225ddbb75b616e57ebd0afd3de4f
K-Minhyeok <gurdl2384@naver.com>
2024-02-14 14:30:10 +0900

    feat: S3 연동 관련 파일 추가

--- src/main/java/com/likelion/GloBuddyBackend/GloBuddyBackendApplication.java
+++ src/main/java/com/likelion/GloBuddyBackend/GloBuddyBackendApplication.java
@@ -2,8 +2,10 @@ package com.likelion.GloBuddyBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GloBuddyBackendApplication {
 
 	public static void main(String[] args) {
