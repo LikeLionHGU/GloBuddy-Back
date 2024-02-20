f43e01d86419b090f1a32bb28892d270f4ab4733
Min_h_bot <74346735+K-Minhyeok@users.noreply.github.com>
2024-02-20 14:26:01 +0900

    feat:MemberRepo 충돌 해결

--- src/main/java/com/likelion/GloBuddyBackend/repository/MemberRepository.java
+++ src/main/java/com/likelion/GloBuddyBackend/repository/MemberRepository.java
@@ -1,12 +1,15 @@
 package com.likelion.GloBuddyBackend.repository;
 
 import com.likelion.GloBuddyBackend.domain.Member;
-import com.likelion.GloBuddyBackend.domain.QMember;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.likelion.GloBuddyBackend.dto.MemberDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
+
 public interface MemberRepository extends JpaRepository<Member, Long>  {
+
+    boolean existsByEmail(String email);
+
+    Member findByEmail(String email);
 
 }
