c0f41af07331bf8bddbdad08fd1782cfda9f59af
Min_h_bot <74346735+K-Minhyeok@users.noreply.github.com>
2024-02-20 14:20:12 +0900

    delete: yml 삭제

--- src/main/java/com/likelion/GloBuddyBackend/repository/MemberRepository.java
+++ src/main/java/com/likelion/GloBuddyBackend/repository/MemberRepository.java
@@ -9,15 +9,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>  {
 
-//    private final JPAQueryFactory query; //
-//    private final QMember member = QMember.member; // 멤버
-
-//    public List<Member> findAll(PostSearchFilter filter, Member member, long offset, int limit) {
-//        return query
-//                .selectFrom(member)
-//                .where()
-//                .fetch();
-//    }
-
-
 }
